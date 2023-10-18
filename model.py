2728ce580bdcf4807db81001c353f4da0e5b53db
politics-in-the-pond <74289147+politics-in-the-pond@users.noreply.github.com>
2023-10-19 00:41:50 +0900

    Update model.py

--- model.py
+++ model.py
@@ -29,41 +29,10 @@ class Transition(nn.Module):
             nn.BatchNorm2d(in_channels),
             nn.ReLU(),
             nn.Conv2d(in_channels, out_channels, 1, stride=1, padding=0, bias=False),
-            #nn.AvgPool2d(2, stride=2)
         )
 
     def forward(self, x):
-        #x = self.attention(x)
         return self.down_sample(x)
-
-class Attention_block(nn.Module):
-    def __init__(self, F_g, F_l, F_int):
-        super(Attention_block, self).__init__()
-        self.W_g = nn.Sequential(
-            nn.Conv2d(F_g, F_int, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(F_int)
-        )
-
-        self.W_x = nn.Sequential(
-            nn.Conv2d(F_l, F_int, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(F_int)
-        )
-
-        self.psi = nn.Sequential(
-            nn.Conv2d(F_int, 1, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(1),
-            nn.Sigmoid()
-        )
-
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, g, x):
-        g1 = self.W_g(g)
-        x1 = self.W_x(x)
-        psi = self.relu(g1 + x1)
-        psi = self.psi(psi)
-
-        return x * psi
 
 class DenseNet(nn.Module):
     def __init__(self, nblocks, growth_rate=12, reduction=0.5, num_classes=4, init_weights=True):
